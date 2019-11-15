89edd4e745f0955df3985083d14eb3601bc3a648
KyleAng <kyleang7.08@gmail.com>
2019-11-15 10:13:38 +0800

    Completed 7, 11 & 12

---
+++ ch01/src/BurmaShave.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class BurmaShave {
+    public static void main(String args[]){
+        JOptionPane.showMessageDialog(null,("The wolf"));
+        JOptionPane.showMessageDialog(null,("Is shaved"));
+        JOptionPane.showMessageDialog(null,("So neat and trim"));
+        JOptionPane.showMessageDialog(null,("Red Riding Hood"));
+        JOptionPane.showMessageDialog(null,("Is chasing him"));
+    }
+}
--- ch01/src/MovieQuoteInfo.java
+++ ch01/src/MovieQuoteInfo.java
@@ -1,2 +1,8 @@
 public class MovieQuoteInfo {
+
+    public static void main(String[] args){
+        System.out.print("\"People Say nothing is impossible, but I do nothing everyday.\" - Winnie The Pooh");
+        System.out.print(", Christopher Robin (2018)");
+
+    }
 }
