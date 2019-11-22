6bffcd378db970a7e542fc30b228ecb7697ad3e9
KyleAng <kyleang7.08@gmail.com>
2019-11-22 08:55:28 +0800

    Completed DebugTwo1 Ch02

--- ch02/DebuggingExercises/DebugTwo1.java
+++ ch02/DebuggingExercises/DebugTwo1.java
@@ -6,10 +6,10 @@ public class DebugTwo1
       double oneDouble = 12.4;
       char oneChar = A;
       System.out.print("The int is ");
-      System.out.pintln(oneInt);
+      System.out.println(oneInt);
       System.out.print("The double is ");
       System.println(oneDouble);
-      System.out.print(The char is ");
+      System.out.print("The char is ");
       System.out.println(oneChar);
    }
 }
